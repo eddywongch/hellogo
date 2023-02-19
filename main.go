7591b68ed85292b845e2c0a485b871f203464688
eddy@yellowtangsoftware.com <eddy@yellowtangsoftware.com>
2023-02-19 12:45:41 -0500

    Created

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eddywongch/stringutils"
+)
+
+const location = "Remote"
+
+var name string
+
+func main() {
+
+	name = "Eddy"
+	from := `Boston`
+	var n int
+
+	//var proverb = "Undefined"
+	//if p, err := proverbs.Nth(4); err == nil {
+	//	proverb = p.Saying
+	//}
+
+	fmt.Printf("Hello, fellow %s Gophers!\n", location)
+	fmt.Printf("My name is %s and I'm from %s.\n", name, from)
+	fmt.Printf("By the time %d o'clock EST comes around, we'll know how to code in Go!\\n", n)
+	//fmt.Printf("Here's a Go proverb: %s\n", proverb)
+	fmt.Println("Let's get started!")
+
+	fmt.Println(stringutils.Upper("Hello Upper"))
+	fmt.Println(stringutils.Lower("Hello Lower"))
+}
